c1ab444b07220ccbda80e3f37f7b93ffac02972b
yubise <josexdrosalesj@gmail.com>
2024-07-13 23:08:44 -0400

    agregando funciones para calcular vth

--- nuevo programa/vth_and_zth.py
+++ nuevo programa/vth_and_zth.py
@@ -11,3 +11,44 @@ def calcular_vth_zth(workbook):
             else:
                 zth_type = "inductivo"
         sheet.cell(row=row[0].row, column=4, value=zth_type)
+        
+def Cantidad_nodos(nodos):#esta funcion devuelve la cantidad de nodos que hay en el problema
+    cant_nodos=1
+    for x in range(len(nodos)):
+        if nodos[x][0]>cant_nodos or nodos[x][1]>cant_nodos:
+            if nodos[x][0]>nodos[x][1]:
+                cant_nodos=nodos[x][0]
+            else:
+                cant_nodos=nodos[x][1]
+    return cant_nodos
+
+def matriz(canti_nodos,Y): #calcula la matriz de admitancias
+    fila=[]
+    matriz_y=[]
+    for x in range(1,canti_nodos):
+        for n in range(1,canti_nodos):
+            b=0
+            for m in Y:
+                if x!=n:
+                    if m[0]==n and m[1]==x:
+                        fila.append(-1*m[2])
+                        break
+                    elif m[0]==x and m[1]==n:
+                        fila.append(-1*m[2])
+                        break
+                else:
+                    fila.append(suma_matriz(n,Y))
+                    break
+                b+=1
+            if b==len(Y):
+                fila.append(0)       
+        matriz_y.append(fila)
+        fila=[]
+    return matriz_y
+                    
+def suma_matriz(n,Y): #calcula los valores de la diagonal de la matriz
+    Y_eq=0
+    for m in Y:
+        if m[0]==n or m[1]==n:
+            Y_eq+=m[2]
+    return Y_eq

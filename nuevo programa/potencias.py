3e40fd5941191cdc13997714468b94c7f24138f1
CESARESTRADA31 <cesarin3107leo@gmail.com>
2024-07-14 23:53:18 -0400

    Creo que esta listo el calculo de potencias, pero falta la funcion para calcular las potencias de las impedancias en serie con fuentes, por favor jefe salvame

--- nuevo programa/potencias.py
+++ nuevo programa/potencias.py
@@ -1,17 +1,45 @@
-lista=[[1,2,5],[2,3,3]]
+import numpy as np
+import openpyxl as px
+import cmath
+#Primero hay que llamar los valores
+from impedancias import calcular_impedancias as impedancia
+
+lista=[[1,2,impedancia],[2,3,impedancia]]
 #colummna 1 nodo i, columna 2 nodo j, columna 3 impedancia
-vth=[1,2,3,54]
-potencias=[]
-for x in lista:
-    pot=(vth[x[0]-1]-vth[x[1]-1])**2/x[2]
-    potencias.append([x[0],x[1],pot])
 
-(vf-vth)*vf/z
 
-vf=[[1,100,5],[2,355,23]]
-#columna 1 nodo, columna 2 voltaje, columna 3 impedancia
-vth[x[0]-1]*(vf[x[0]]-vth[x[0]-1])/vf[2] #cmath
+#Operaciones:
+#1)Potencias de impedancias entre nodos
+def Calcular_potencias_impedancias():
+    vth=[1,2,3,54]
+    potencias_z=[]
+    for x in lista:pot=(vth[x[0]-1]-vth[x[1]-1])**2/x[2]
+    potencias_z.append([x[0],x[1],pot])
+    return potencias_z
 
-vth*i #conjugado
-def funcion(impedancias, vth, vfuente, ifuente):
+  #2)Potencia de la fuente
+def Calcular_potencia_voltaje(vth,vf):
+   
+    vth=[1,2,3,54]
+    potencias_v=[]
+    #columna 1 nodo, columna 2 voltaje, columna 3 impedancia
+    for x in lista:pot=vth[x[0]-1]*(vf[x[0]]-vth[x[0]-1])/vf[2]
+    potencias_v.append([x[0],x[1],pot])
+    return potencias_v
+    
+
+from I_fuente import calcular_corrientes_fuente as I
+#3)Potencia de la fuente de corriente
+i=I
+ic= i.conjugate() #Corriente conjugada
+def calcular_potencia_corriente(vth):
+    potencias_i=[]
+    Pi=vth*ic 
+    potencias_i.append(Pi)
+    return Pi
+
+
+
+#4)Potencias de impedancias en serie con una fuente:
+  #nose :c
     
